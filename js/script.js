a74db46fe1878033c46c9a30069d70cf059d2b80
BilawalGul <bilwalawan747@gmail.com>
2019-09-18 00:05:23 +0500

    create following page and add javascript toggle

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function myFunction(){
+
+	var x = document.getElementById("createpin");
+	var y = document.getElementById("changetext");
+
+	if(x.style.display === 'block' && y.innerHTML === '+'){
+		x.style.display = 'none';
+		y.innerHTML === 'x';
+
+	}else{
+		x.style.display = 'block';
+		y.innerHTML === '+';
+	}
+
+}
+
+function anFunction(){
+
+	var x = document.getElementById("createpind");
+	var y = document.getElementById("changetext");
+
+	if(x.style.display === 'block' && y.innerHTML === '+'){
+		x.style.display = 'none';
+		y.innerHTML === 'x';
+
+	}else{
+		x.style.display = 'block';
+		y.innerHTML === '+';
+	}
+
+}
